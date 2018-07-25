1fcf5308dd5827b2f8418cee03df86e5bafadf57
gdymind <gdymind@gmail.com>
2018-07-26 00:36:41 +0800

    Add matlab script to compute Opacity

---
+++ src/solveOpacity.m
@@ -0,0 +1,23 @@
+segmentNum = size(D, 1);
+
+I = eye(segmentNum);
+W = ((I-G)^coff(5)) * H * G;
+
+Q = coff(1) * I;
+Q = Q + coff(2) * (W * W.');
+Q = Q + coff(3) * (W.' * W);
+Q = Q + coff(4) * (D.' * D);
+
+c = -1 * ones(segmentNum, 1);
+
+lb = zeros(segmentNum, 0);
+ub = ones(sgementNum, 1);
+
+
+if (size(O, 1) == 1)
+    O = O.';
+end
+O = quadprog(Q, c, [], [], [], [], lb, ub, O);
+O = O.';
+
+flag = true;
